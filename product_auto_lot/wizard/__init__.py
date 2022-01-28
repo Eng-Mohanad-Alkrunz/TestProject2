7c799a2217c1434d03ad595d98be54c7a17edaf4
Eng-Mohanad-Alkrunz <muhannd@ahdtech.com>
2022-01-28 22:33:44 +0200

    REmove Comments 15

--- product_auto_lot/wizard/__init__.py
+++
@@ -1,9 +0,0 @@
-
-
-from . import stock_pack_operation
-from . import mrp_generate_pallet
-from . import mrp_generate_lot
-from . import mrp_pack_pallet
-from . import mrp_unpack_pallet
-from . import mrp_import_packages_wizard
-from . import mrp_workorder_confirmation
